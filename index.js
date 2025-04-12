ba73b86566b6010989d59ea60d24326fbd7d1f4e
Levis155 <levismbui@gmail.com>
2025-04-12 12:18:23 +0300

    implement login functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { PrismaClient } from '@prisma/client'
 import bcrypt from "bcryptjs";
 import cors from "cors";
+import jwt from "jsonwebtoken"
 import { validateEmailAndUsername } from "./middleware/validateEmailAndUsername.js";
 
 const app = express();
@@ -10,7 +11,8 @@ const client = new PrismaClient()
 app.use(express.json())
 app.use(cors({
     origin: "http://localhost:5173",
-    methods: ["POST", "GET", "PUT", "PATCH", "DELETE"]
+    methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
+    credentials: true
 }))
 
 app.post("/auth/register", validateEmailAndUsername, async(req, res) => {
@@ -37,6 +39,40 @@ app.post("/auth/register", validateEmailAndUsername, async(req, res) => {
             message: "Something went wrong. Please try again."
         })
     }
+})
+
+app.post("/auth/login", async (req, res) => {
+    const { identifier, password } = req.body;
+
+   try{
+    const user = await client.user.findFirst({where: {OR: [{emailAddress: identifier}, {userName: identifier}]}})
+
+    if(!user) {
+        return res.status(401).json({message:"Wrong login credentials."})
+    }
+
+    const theyMatch = bcrypt.compare(password, user.password);
+
+    if(!theyMatch) {
+        return res.status(401).json({message: "Wrong login credentials."})
+    }
+
+    const jwtPayLoad = {
+        userId: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName
+    }
+
+    const token = jwt.sign(jwtPayLoad, process.env.JWT_SECRET_KEY);
+    res.status(200).cookie("blogitAuthToken", token).json({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        emailAddress: user.emailAddress,
+        username: user.userName
+    })
+   } catch(e) {
+    res.status(500).json({message: "Something went wrong."})
+   } 
 })
 
 let port = process.env.PORT || 3000;
