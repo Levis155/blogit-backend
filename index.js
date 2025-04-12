c4626ab38f38ac1c4a73c3ae6a5c5045a0dc1105
Levis155 <levismbui@gmail.com>
2025-04-12 09:48:50 +0300

    implement username and email duplication protection

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from "express";
 import { PrismaClient } from '@prisma/client'
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
+import cors from "cors";
+import { validateEmailAndUsername } from "./middleware/validateEmailAndUsername.js";
 
 const app = express();
 const client = new PrismaClient()
 
 app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["POST", "GET", "PUT", "PATCH", "DELETE"]
+}))
 
-app.post("/auth/register", async(req, res) => {
+app.post("/auth/register", validateEmailAndUsername, async(req, res) => {
     const{firstName, lastName, emailAddress, userName, password} = req.body;
 
     const hashedPassword = await bcrypt.hash(password, 12)
@@ -24,8 +30,7 @@ app.post("/auth/register", async(req, res) => {
         })
 
         res.status(201).json({
-            message: "User registered successfully",
-            data: newUser
+            message: "User registered successfully"
         })
     } catch(e) {
         res.status(500).json({
