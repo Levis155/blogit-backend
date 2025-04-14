739d64d47a1cf8eb9c4f8b12a6400048b0eb8cdd
Levis155 <levismbui@gmail.com>
2025-04-14 06:03:55 +0300

    implement update functionality

--- Controllers/blogsController.js
+++ Controllers/blogsController.js
@@ -14,9 +14,69 @@ export const createBlog = async (req, res) => {
                 authorId
             }
         })
-        res.status(201).json({message: "blog created successfully!"})
+        res.status(201).json({newBlog})
     } catch(e) {
         console.log(e);
         res.status(500).json({message:"Something went wrong"})
+    }
+}
+
+export const getBlog = async (req, res) => {
+    try{
+        const authorId = req.user.userId;
+        const { blogId } = req.params;
+
+        const blog = await client.blog.findFirst({
+            where:{
+                authorId,
+                id: blogId,
+                isDeleted: false
+            }
+        })
+        
+        if(blog) {
+            return res.status(200).json(blog)
+        }
+
+        res.status(404).json({message: "blog not found."})
+    } catch(e) {
+        res.status(500).json({message: "Something went wrong."})
+    }
+}
+
+export const getallBlogsByUser = async(req, res) => {
+    try{
+        const authorId = req.user.userId;
+        const blogs = await client.blog.findMany({
+            where:{
+                AND:[{authorId}, {isDeleted: false}]
+            }
+        })
+        console.log(blogs)
+        res.status(200).json(blogs);
+    } catch(e) {
+        res.status(500).json({message: "Something went wrong."})
+    }
+}
+
+export const updateBlog = async (req, res) => {
+    try{
+        const authorId = req.user.userId;
+        const { blogId } = req.params;
+        const {title, excerpt} = req.body;
+        const updatedBlog = await client.blog.update({
+            where: {
+                authorId,
+                id: blogId
+            },
+            data: {
+                title: title && title,
+                excerpt: excerpt && excerpt,
+            }
+        })
+
+        res.status(200).json({message: "Blog updated successfully."})
+    } catch (e) {
+        res.status(500).json({message: "Something went wrong."})
     }
 }
--- Routes/blogsRoutes.js
+++ Routes/blogsRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createBlog } from "../Controllers/blogsController.js";
+import { createBlog, getBlog, getallBlogsByUser, updateBlog } from "../Controllers/blogsController.js";
 import verifyUser from "../middleware/verifyUser.js";
 
 const router = Router();
 
-router.route("/").post(verifyUser, createBlog)
+router.route("/").post(verifyUser, createBlog).get(verifyUser, getallBlogsByUser);
+router.route("/:blogId").get(verifyUser, getBlog).patch(verifyUser, updateBlog);
 
 export default router;
