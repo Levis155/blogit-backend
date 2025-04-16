7eb205a40ce9e348273ca9154152821e2c942fba
Levis155 <levismbui@gmail.com>
2025-04-16 06:13:41 +0300

    edit origin url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://blogit-frontend-beta.vercel.app",
+    origin: ["http://localhost:5173", "https://blogit-frontend-beta.vercel.app",],
     methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
     credentials: true
 }))
