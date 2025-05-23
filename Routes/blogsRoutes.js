6afd5ebd589ce8ac399db67e55fa1dbf8bdb4618
Levis155 <levismbui@gmail.com>
2025-05-23 16:30:31 +0300

    modify endpoint for getting blogs by user and all blogs

--- Routes/blogsRoutes.js
+++ Routes/blogsRoutes.js
@@ -11,11 +11,8 @@ import verifyUser from "../middleware/verifyUser.js";
 
 const router = Router();
 
-router
-  .route("/")
-  .post(verifyUser, createBlog)
-  .get(verifyUser, getallBlogsByUser);
-router.route("/all-blogs").get(verifyUser, getAllBlogs);
+router.route("/").post(verifyUser, createBlog).get(verifyUser, getAllBlogs);
+router.route("/user").get(verifyUser, getallBlogsByUser);
 router
   .route("/:blogId")
   .get(verifyUser, getBlog)
